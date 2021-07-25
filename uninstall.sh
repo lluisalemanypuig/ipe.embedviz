a163f470984f8581424e6200de882702cec49689
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2021-07-25 09:30:30 +0200

    Updated uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,10 +9,13 @@ rm -f ~/.ipe/ipelets/ev_auxiliary_functions.lua
 rm -f ~/.ipe/ipelets/ev_parse_input_data.lua
 rm -f ~/.ipe/ipelets/ev_queue.lua
 
-# LINEAR EMBEDDING VISUALIZER
+rm -f ~/.ipe/ipelets/lev_draw_input_data.lua
+rm -f ~/.ipe/ipelets/cev_draw_input_data.lua
 
+rm -f ~/.ipe/ipelets/ev_embedding_visualizer.lua
+
+# remove the old lua files
 rm -f ~/.ipe/ipelets/lev_draw_input_data.lua
 rm -f ~/.ipe/ipelets/lev_embedding_visualizer.lua
-# remove the old lua files
-rm -f ~/.ipe/ipelets/ev_draw_input_data.lua
-rm -f ~/.ipe/ipelets/ev_embedding_visualizer.lua
+rm -f ~/.ipe/ipelets/cev_draw_input_data.lua
+rm -f ~/.ipe/ipelets/cev_embedding_visualizer.lua
